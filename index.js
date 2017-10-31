3ba1add5657ff77713ae43440105f7de9290a692
Higor Diego <higordiegoti@gmail.com>
2017-10-31 13:20:38 -0300

    subindo exerccio

---
+++ index.js
@@ -0,0 +1,23 @@
+// var => 'Variavel global'
+// const => 'Constante'
+// let => 'Variavel de ambiente'
+// typeof => consultar qual o tipo dessa variavel
+// estudar lei da negação, if ternario (execicio: 01)
+// == comparação entre variaveis
+// === comparação de tipagem de variaveis
+// Pure Functions
+// side effectorys
+// versão antiga
+// function soma(primeiroNumero, segundoNumero){
+//     return primeiroNumero + segundoNumero
+// } 
+// versão nova
+// const arrowFunction = (primeiroNumero, segundoNumero) => primeiroNumero + segundoNumero
+// var resultado = soma(10, 20)
+// var resultado2 = arrowFunction(10,20)
+// console.log(resultado)
+// console.log(resultado2)
+// antiga
+// console.log(primeiroNome+'-'+segundoNome)
+// nova
+// console.log(`${primeiroNome}-${segundoNome}`)
